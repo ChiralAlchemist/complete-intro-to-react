34cc3f684cd4ce71c5141c4c1d24106b24ccf491
Joseph Ashby <jashby473@gmail.com>
2016-09-09 00:01:14 -0700

    done

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const ReactRedux = require('react-redux')
 const Provider = ReactRedux.Provider
 const Store = require('./js/Store.jsx')
 const store = Store.store
-const _ =  require('lodash')
+const _ = require('lodash')
 const fs = require('fs')
 const port = 5050
 const baseTemplate = fs.readFileSync('./index.html')
@@ -23,8 +23,8 @@ const app = express()
 app.use('/public', express.static('./public'))
 
 app.use((req, res) => {
-  match({ routes: Routes(), location: req}, (error, redirectLocation, renderProps) => {
-    if(error) {
+  match({ routes: Routes, location: req.url }, (error, redirectLocation, renderProps) => {
+    if (error) {
       res.status(500).send(error.message)
     } else if (redirectLocation) {
       res.redirect(302, redirectLocation.pathname + redirectLocation.search)
@@ -36,7 +36,7 @@ app.use((req, res) => {
       )
       res.status(200).send(template({ body }))
     } else {
-      res.status(404).send('not found')
+      res.status(404).send('Not found')
     }
   })
 })
